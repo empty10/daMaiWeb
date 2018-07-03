a06efc41a88f39fa364aecac258a04e9dda8d6d1
lshn2018 <836321280@qq.com>
2018-07-03 18:03:11 +0800

    l

--- PROJECT/src/routes/Detail.js
+++ PROJECT/src/routes/Detail.js
@@ -34,14 +34,24 @@ class Detail extends React.Component {
                             <span className='noticeTitle'>演出时长</span>
                             <span className='noticeCont'>约120分钟（以现场时间为准）</span>
                         </li>
+                        <li className='noticeItem'>
+                            <span className='noticeTitle'>入场时间</span>
+                            <span className='noticeCont'>演出前约30分钟</span>
+                        </li>
+                        <li className='noticeItem'>
+                            <span className='noticeTitle'>限购说明</span>
+                            <span className='noticeCont'>选座购买每单限6张，立即购买每单限20张</span>
+                        </li>
+                        <li className='noticeItem'>
+                            <span className='noticeTitle'>儿童说明</span>
+                            <span className='noticeCont'>1.2米以上凭成人票入场，1.2米以下谢绝入场</span>
+                        </li>
                     </div>
                     <div className='Notice_more'>
                         <a href=""> 更多图文详情</a>
                     </div>
-
                 </div>
             </div>
-
         </div>;
     }
 }

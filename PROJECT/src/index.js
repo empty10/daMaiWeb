b0db56ac9eb52fe9453f309eab611de881b35ec6
lshn2018 <836321280@qq.com>
2018-07-03 12:26:52 +0800

    update index

--- PROJECT/src/index.js
+++ PROJECT/src/index.js
@@ -8,41 +8,36 @@ import {Provider} from 'react-redux';
 import store from './store/index';
 
 /*ANTD*/
-import {LocaleProvider} from 'antd';
-import zh_CN from 'antd/lib/locale-provider/zh_CN';
+
 
 /*IMPORT CSS*/
 import './static/css/reset.min.css';
-import './static/css/common.less';
+
 
 /*IMPORT COMPONENT*/
-import NavTop from './component/NavTop';
-import NavBottom from './component/NavBottom';
-import Home from './routes/Home';
-import Mycourse from './routes/Mycourse';
-import Person from './routes/Person';
+
 
 /*RENDER*/
 render(<Provider store={store}>
     <HashRouter>
-    <LocaleProvider locale={zh_CN}>
-    <div>
-    {/*HEADER*/}
-    <NavTop/>
+        <LocaleProvider locale={zh_CN}>
+            <div>
+                {/*HEADER*/}
+                <NavTop/>
 
-    {/*MAIN=>ROUTE*/}
-    <main className='container'>
-    <Switch>
-    <Route path='/course' component={Home}/>
-<Route path='/mycourse' component={Mycourse}/>
-<Route path='/person' component={Person}/>
-<Redirect to='/course'/>
-    </Switch>
-    </main>
+                {/*MAIN=>ROUTE*/}
+                <main className='container'>
+                    <Switch>
+                        <Route path='/course' component={Home}/>
+                        <Route path='/mycourse' component={Mycourse}/>
+                        <Route path='/person' component={Person}/>
+                        <Redirect to='/course'/>
+                    </Switch>
+                </main>
 
-{/*FOOTER*/}
-<NavBottom/>
-</div>
-</LocaleProvider>
-</HashRouter>
+                {/*FOOTER*/}
+                <NavBottom/>
+            </div>
+        </LocaleProvider>
+    </HashRouter>
 </Provider>, root);

1918b54a15e36415ec3f5e44d8a7262243abd340
lshn2018 <836321280@qq.com>
2018-07-04 10:33:50 +0800

    update detail

--- PROJECT/src/routes/Detail.js
+++ PROJECT/src/routes/Detail.js
@@ -3,25 +3,30 @@ import {connect} from 'react-redux';
 import '../static/css/detail.less';
 
 class Detail extends React.Component {
-    constructor(props,context) {
-        super(props,context);
+    constructor(props, context) {
+        super(props, context);
     }
 
     render() {
         return <div className='performBd'>
             <div className='headerBox'>
-                <img src="../static/images/singer.jpg" alt=""/>
-                <p className='title'>CHARLIE PUTH VOICENOTES TOUR HONG KONG </p>
-                <p className='city'>香港</p>
-                <p className='price'>
-                    540-1350
-                    <span className='priceUnit'>元</span>
-                </p>
+                <div className='back'> </div>
+                <div className='itemBox'>
+                    <div className='pic'> <img src="https://pimg.dmcdn.cn/perform/project/1563/156385_n.jpg?_t=1530166503931?1530547884494" alt=""/></div>
+                    <p className='title'>CHARLIE PUTH VOICENOTES TOUR HONG KONG </p>
+                    <p className='city'>香港</p>
+                    <p className='price'>
+                        540-1350
+                        <span className='priceUnit'> 元</span>
+                    </p>
+                </div>
             </div>
-            <div className='holdTime'>2018.11.04 20:00 </div>
-            <div className='holdPlace'>亚洲国际博览馆 </div>
+            <div className='holdTime'>
+                <div className='left'>2018.11.04 20:00</div>
+            </div>
+            <div className='holdPlace'>亚洲国际博览馆</div>
             <div className='introduce'>
-                <h1 className='desTitle' > 介绍</h1>
+                <h1 className='desTitle'> 介绍</h1>
                 <div className='desShort'>Charlie Puth于11月4日假亚洲国际博览馆Hall 10举行只此一场的演出！</div>
                 <div className='desMore'>
                     <a href=""> 更多图文详情</a>
@@ -29,7 +34,7 @@ class Detail extends React.Component {
             </div>
             <div className='noticeOfPurchase'>
                 <h1 className='noticeTitle'>购票须知</h1>
-                <div className='infoPurchase'>
+                <div className='infoPurchase ' >
                     <li className='noticeItem'>
                         <span className='noticeTitle'>演出时长</span>
                         <span className='noticeCont'>约120分钟（以现场时间为准）</span>
@@ -47,12 +52,12 @@ class Detail extends React.Component {
                         <span className='noticeCont'>1.2米以上凭成人票入场，1.2米以下谢绝入场</span>
                     </li>
                 </div>
-                <div className='Notice_more'>
-                    <a href=""> 更多图文详情</a>
+                <div className='noticeMore'>
+                    <a href=""> 更多购票须知</a>
                 </div>
             </div>
         </div>;
     }
 }
 
-export default connect( )(Detail);
+export default connect()(Detail);
